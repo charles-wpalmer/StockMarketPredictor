c443c302af8afe0558c30bd4f0e162c0a5c004da
Charles Palmer <charlespalmer@Charless-Air.home>
2018-02-13 23:01:25 +0000

    Moved code out of PolarityBasic into the SentimentAnalysis class and main class

--- src/chp38/Main.java
+++ src/chp38/Main.java
@@ -1,7 +1,16 @@
 package chp38;
 
-public class Main {
-    public static void main(String args[]){
+import chp38.SentimentAnalysis.SentimentAnalysis;
 
+public class Main {
+    public static void main(String[] args) {
+        String[] dir = new String[1];
+        dir[0] = "../";
+        try {
+            new SentimentAnalysis(dir).run();
+        } catch (Throwable t) {
+            System.out.println("Thrown: " + t);
+            t.printStackTrace(System.out);
+        }
     }
 }
