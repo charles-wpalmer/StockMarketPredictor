8c7c5aa0194d10a721be56402671bacc11055b53
Charles Palmer <charlespalmer@Charless-MacBook-Air.local>
2018-02-14 09:28:14 +0000

    Added methods to AlphaVantage

--- src/chp38/APIHandler/AlphaVantage.java
+++ src/chp38/APIHandler/AlphaVantage.java
@@ -1,4 +1,33 @@
-package chp38.SentimentAnalysis.APIHandler;
+package chp38.APIHandler;
 
+/**
+ *  A class to handle the API calls to Alpha vantage to gain
+ *  Stock market prices.
+ *
+ * @author Charles Palmer
+ * @since 2018-13-02
+ */
 public class AlphaVantage {
+    AlphaVantage(){
+
+    }
+
+    /**
+     * Get the past daily prices for a given comodity
+     *
+     * @param commodity the type of commodity
+     */
+    public void getDailyPrices(String commodity){
+
+    }
+
+    /**
+     * Get the past simple moving average for a given
+     * comodity
+     *
+     * @param commodity the type of commodity
+     */
+    public void getSMA(String commodity){
+
+    }
 }
