3c878927a0f021d9e577a03aaa922a40c7f9bdba
Charles <chp38@aber.ac.uk>
2018-04-05 22:51:16 +0100

    Work on the serverAPI class - sending the market prediction info to the server

--- src/chp38/APIHandler/ServerAPI.java
+++ src/chp38/APIHandler/ServerAPI.java
@@ -1,4 +1,43 @@
 package chp38.APIHandler;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Class to communicate with the Server API
+ *
+ * @author charlespalmer
+ */
 public class ServerAPI {
+
+    /**
+     * String to hold the API URL
+     */
+    private final String requestURL = "http://markets.development/api/markets";
+
+    /**
+     * Variable for the connection
+     */
+    private static HttpURLConnection con;
+
+    /**
+     * Method to handle sending the prediction information off to the server
+     * 
+     * @throws IOException
+     */
+    public void sendMarketPrediction() throws IOException {
+
+        URL url = new URL(this.requestURL);
+        URLConnection conn = url.openConnection();
+        conn.setDoOutput(true);
+        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+
+        writer.write("market_name=Coca&date=2018-04-04&prediction=Increase&prev_day_high=100.4&prev_day_low=98.4");
+        writer.flush();
+
+        writer.close();
+    }
 }
